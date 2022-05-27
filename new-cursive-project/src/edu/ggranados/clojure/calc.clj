6087fdd66864f296e7eb474eca12ec621840d63a
Guillermo Granados <guillermo.granados@outlook.com>
2022-05-26 22:53:57 -0400

    [Clojure basics] added simple arithmetic calculator

---
+++ new-cursive-project/src/edu/ggranados/clojure/calc.clj
@@ -0,0 +1,13 @@
+(ns edu.ggranados.clojure.calc)
+
+(defn sum [a b]
+     (+ a b))
+
+(defn sub [a b]
+  (- a b))
+
+(defn mul [a b]
+  (* a b))
+
+(defn div [a b]
+  (float (/ a b)))
--- new-cursive-project/src/main/app.clj
+++ new-cursive-project/src/main/app.clj
@@ -1,9 +1,19 @@
 (ns main.app)
 (require '[new-cursive-project.hello :as hello])
+(require '[edu.ggranados.clojure.calc :as calc])
 
 (defn main []
   (println "Welcome to my first Clojure app!")
   (hello/foo "Clojure!")
+
+  (def a 2)
+  (def b 3)
+
+  (println  "Sum of " a "+" b "=" (calc/sum a b))
+  (println "Subtraction of " a "-" b "=" (calc/sub a b))
+  (println "Multiplication of " a "*" b "=" (calc/mul a b))
+  (println "Division of " a "%" b "=" (calc/div a b))
+
 )
 
 (main)
