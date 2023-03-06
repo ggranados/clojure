ec1b6b0e2a77284c506711b76166a5a15e2cc688
Guillermo Granados <guillermo.granados@outlook.com>
2023-03-05 22:56:23 -0300

    [factorial] changed to use enhanced recursive

--- exercises/src/exercises/core.clj
+++ exercises/src/exercises/core.clj
@@ -1,6 +1,6 @@
 (ns exercises.core
   (:require [exercises.factorial :as fact]))
 
-(def n 5)
+(def n 100)
 
-(println "Factorial of " n " is =" (fact/fact-basic n))
+(println "Factorial of " n " is =" (fact/fact-recur n))
